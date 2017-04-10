cf76c22e123db3809f7fe881a1e86708ae4e9567
emily <emshilton@gmail.com>
2017-04-10 02:40:33 -0400

    working

--- cpu-usage.py
+++ cpu-usage.py
@@ -1,35 +1,34 @@
+#This program allows you to output your computer's current CPU usage in a readable format
+
+#psutil is a library for retrieveing inormation about processes and system utilization
 import psutil
-psutil.cpu_times()
-scputimes(user=3961.46, nice=169.729, system=2150.659, idle=16900.540, iowait=629.59, irq=0.0, softirq=19.42, steal=0.0, guest=0, nice=0.0)
 
- for x in range(3):
-...     psutil.cpu_percent(interval=1)
-...
-4.0
-5.9
-3.8
+#Every attribute of psutil represents seconds the CPU has spent in a given mode
 
-for x in range(3):
-...     psutil.cpu_percent(interval=1, percpu=True)
-...
-[4.0, 6.9, 3.7, 9.2]
-[7.0, 8.5, 2.4, 2.1]
-[1.2, 9.0, 9.9, 7.2]
+#User is time spent by normal processes executing in user mode
+userCPU = psutil.cpu_times().user
 
-for x in range(3):
-...     psutil.cpu_times_percent(interval=1, percpu=False)
-...
-scputimes(user=1.5, nice=0.0, system=0.5, idle=96.5, iowait=1.5, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guest_nice=0.0)
-scputimes(user=1.0, nice=0.0, system=0.0, idle=99.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guest_nice=0.0)
-scputimes(user=2.0, nice=0.0, system=0.0, idle=98.0, iowait=0.0, irq=0.0, softirq=0.0, steal=0.0, guest=0.0, guest_nice=0.0)
+if userCPU < 60:
+	print 'Time spent in normal processes: ', userCPU , 'sec'
+else:
+	print 'Time spent in normal processes: ', str(round(userCPU/60, 2)) , 'mins'
 
-psutil.cpu_count()
-4
-psutil.cpu_count(logical=False)
-2
 
-psutil.cpu_stats()
-scpustats(ctx_switches=20455687, interrupts=6598984, soft_interrupts=2134212, syscalls=0)
+#Idle is time spent doing nothing 
+idleCPU = psutil.cpu_times().idle
 
-psutil.cpu_freq()
-scpufreq(current=931.42925, min=800.0, max=3500.0)
+if idleCPU < 60:
+	print 'Time spent doing nothing: ', idleCPU , 'sec'
+else:
+	print 'Time spent doing nothing: ', str(round(idleCPU/60, 2)) , 'mins'
+
+#System is time spent by processes executing in kernel mode
+systemCPU = psutil.cpu_times().system
+
+if systemCPU < 60:
+	print 'Time spent executing in kernel mode: ', systemCPU , 'sec'
+else:
+	print 'Time spent executing in kernel mode: ', str(round(systemCPU/60, 2)) , 'mins'
+
+
+ 
